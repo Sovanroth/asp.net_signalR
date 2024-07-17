982c2a5a8fc6566a2401245f5f272fa6a4428a9d
Sovanroth13 <sovanroth016@gmail.com>
2024-07-17 13:16:51 +0700

    update

--- obj/Debug/net8.0/SignalRChat.AssemblyInfo.cs
+++ obj/Debug/net8.0/SignalRChat.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SignalRChat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+daee8a7ae43f8682dbb31dd016dea02e10951de2")]
 [assembly: System.Reflection.AssemblyProductAttribute("SignalRChat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SignalRChat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
